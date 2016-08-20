04a4ac26b1da9c7187af05e47f3fbeac2605e208
boosawesome <boosawesome@gmail.com>
2016-08-20 21:16:44 +1200

    Add files via upload

--- Cluedo/src/ui/BoardCanvas.java
+++ Cluedo/src/ui/BoardCanvas.java
@@ -6,6 +6,7 @@ import java.awt.Graphics;
 
 public class BoardCanvas extends Canvas{
 	
+	private final int squareSize = 10;
 	public final int numSquare = 25;
 	Square[][] squares;
 	
@@ -13,14 +14,124 @@ public class BoardCanvas extends Canvas{
 		this.setBackground(Color.BLACK);
 		squares = new Square[numSquare][numSquare];
 		
-		
-		
 	}
 	
 	
 	public void paint(Graphics g){
+		int width = 240;
+		int height = 250;
+		
+		g.setColor(Color.GRAY);
+		g.fillRect(0, 0, width, height);
+		drawSquares(g);
+		drawRooms(g);
 		
 	}
 	
+	public void drawSquares(Graphics g){
+		g.setColor(Color.BLACK);
+		
+		for(int x = 0; x < 24; x++){
+			for(int y = 0; y < 25; y++){
+				g.drawRect(x*10, y*10, squareSize, squareSize);
+			}
+		}
+	}
+	
+	public void drawRooms(Graphics g){
+		g.setColor(Color.WHITE);
+		
+		//Kitchen
+		for(int x = 0; x <= 5; x++){
+			for(int y = 1; y <= 6 ; y++){
+				g.fillRect(x*10, y*10, squareSize, squareSize);
+			}
+		}
+		//Ball Room
+		for(int x = 9; x <= 16; x++){
+			for(int y = 2; y <= 7; y++){
+				g.fillRect(x*10, y*10, squareSize, squareSize);
+			}
+		}
+		for(int x = 11; x <= 14; x++){
+			g.fillRect(x*10, 10, squareSize, squareSize);
+		}
+		//Conservatory
+		for(int x = 19; x <= 23; x++){
+			for(int y = 1; y <= 4; y++){
+				g.fillRect(x*10, y*10, squareSize, squareSize);
+			}
+		}
+		for(int x = 20; x <= 23; x++){
+				g.fillRect(x*10, 50, squareSize, squareSize);			
+		}
+		//Dining Room
+		for(int x = 0; x <= 8; x++){
+			for(int y = 11; y <= 16; y++){
+				g.fillRect(x*10, y*10, squareSize, squareSize);
+			}
+		}
+		for(int x = 0; x <= 5; x++){
+			g.fillRect(x*10, 100, squareSize, squareSize);
+		}
+		//Billiard Room
+		for(int x = 19; x <= 23; x++){
+			for(int y = 8; y <= 12; y++){
+				g.fillRect(x*10, y*10, squareSize, squareSize);
+			}
+		}
+		//Library
+		for(int x = 18; x <= 23; x++){
+			for(int y = 14; y <= 18; y++){
+				g.fillRect(x*10, y*10, squareSize, squareSize);
+			}
+		}
+		//Lounge
+		for(int x = 0; x <= 6; x++){
+			for(int y = 19; y <= 24; y++){
+				g.fillRect(x*10, y*10, squareSize, squareSize);
+			}
+		}
+		//Hall
+		for(int x = 18; x <= 24; x++){
+			for(int y = 9; y <= 14; y++){
+				g.fillRect(x*10, y*10, squareSize, squareSize);
+			}
+		}
+		//Study
+		for(int x = 17; x <= 23; x++){
+			for(int y = 21; y <= 24; y++){
+				g.fillRect(x*10, y*10, squareSize, squareSize);
+			}
+		}
+		g.setColor(Color.GREEN);
+		g.fillRect(0, 70, squareSize, squareSize);
+		g.fillRect(60, 10, squareSize, squareSize);
+		g.fillRect(180, 10, squareSize, squareSize);
+		g.fillRect(230, 60, squareSize, squareSize);
+		g.fillRect(0,  90, squareSize, squareSize);
+		g.fillRect(230, 80, squareSize, squareSize);
+		g.fillRect(0, 160, squareSize, squareSize);
+		g.fillRect(0, 180, squareSize, squareSize);
+		g.fillRect(230, 130, squareSize, squareSize);
+		g.fillRect(230, 140, squareSize, squareSize);
+		g.fillRect(230, 180, squareSize, squareSize);
+		g.fillRect(230, 200, squareSize, squareSize);
+		g.fillRect(60, 240, squareSize, squareSize);
+		g.fillRect(80, 240, squareSize, squareSize);
+		g.fillRect(150, 240, squareSize, squareSize);
+		for(int x = 0; x <= 23; x++){
+			g.fillRect(x*10, 0, squareSize, squareSize);
+		}
+		
+		g.setColor(Color.ORANGE);
+		g.fillRect(50, 10, squareSize, squareSize);
+		g.fillRect(0,  190, squareSize, squareSize);
+		g.fillRect(220, 60, squareSize, squareSize);
+		g.fillRect(210, 230, squareSize, squareSize);
+		
+		g.setColor(Color.RED);
+		
+	}
 	
 }
